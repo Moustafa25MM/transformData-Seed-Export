6fac15bae9bf2493031feffbbc7c2a8c5ce7fbf3
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-04-09 02:33:20 +0200

    seed data

---
+++ src/helpers/seedData.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import { faker } from '@faker-js/faker';
+import BrandModel from '../models/Brand.model';
+
+export const seedDatabase = async () => {
+  try {
+
+    for (let i = 0; i < 10; i++) {
+      const brand = new BrandModel({
+        brandName: faker.company.name(),
+        yearFounded: faker.date.between({from: '1600-01-01',to: new Date().getFullYear().toString()}).getFullYear(),
+        headquarters: `${faker.location.city()}, ${faker.location.country()}`,
+        numberOfLocations: faker.number.int({ min: 1, max: 10000 }),
+      });
+
+      await brand.save();
+    }
+
+    console.log('Database seeded!');
+  } catch (error) {
+    console.error('Error seeding database:', error);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { seedDatabase } from "./helpers/seedData";
 import { transformData } from "./helpers/transformData";
 import { connectToMongoDB } from "./mongooseConnection";
 
 connectToMongoDB().then(async ()=>{
     await transformData();
+    await seedDatabase();
 })
